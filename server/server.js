a9aee72fdbd7f35b4ea26a7387e422b41a010256
golnazabbasii <golnazaba3066@gmail.com>
2020-09-26 17:56:11 +0330

    test

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 const publicPath = path.join(__dirname, '..' , 'build');
 
-// console.log(publicPath);
+console.log(publicPath);
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
